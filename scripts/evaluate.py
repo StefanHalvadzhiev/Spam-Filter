0068fac61c793aec9aa1fb2c0ab03887a22fc5df
Stefan Halvadzhiev <feneris360@gmail.com>
2023-05-27 10:38:22 +0300

    Fix evaluate function. It shows the correct calculation type. Add changes to notebooks.

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -1,7 +1,7 @@
-def evaluate(number1: str, number2: str) -> float:
+def evaluate(number1: str, number2: str, calculation_type: str) -> float:
     print(
         f"Number of mislabeled points out of a total {number1} points : {number2}"
     )
     percentage = 100 - number2 / number1 * 100
-    print(f"Accuracy of Gaussian model: {percentage}")
+    print(f"Accuracy of {calculation_type} model: {percentage}")
     return percentage
